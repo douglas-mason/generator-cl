140fd20f83791ed58ef3503fbc178b2c5b93cffc
Ben Southgate <bsouthga@gmail.com>
2017-02-13 15:19:36 -0500

    templates

--- src/comp/index.ts
+++ src/comp/index.ts
@@ -1,14 +1,35 @@
-import Generator = require('yeoman-generator');
+import GeneratorClass = require('yeoman-generator');
+import * as _ from 'lodash';
 
 
+class ComponentGenerator extends GeneratorClass {
 
-/**
- * old style commonjs export for yeoman
- */
-export = class extends Generator {
+  options: {
+    name: string;
+  };
 
-  method1() {
-    console.log('component');
+  constructor(args: any, opts: any) {
+    super(args, opts);
+    this.argument('name', { type: String, required: true });
+  }
+
+  writing() {
+    const name = this.options.name;
+    console.log(`In writing method -- ${name}`);
+    this.fs.copyTpl(
+      this.templatePath('template.component.ts.template'),
+      this.destinationPath(`test/${name}.component.ts`),
+      {
+        componentName: name,
+        componentNameCamelCase: _.camelCase(name),
+        controllerName: `${_.capitalize(_.camelCase(name))}Controller`,
+        templateUrl: 'TEST',
+        baseControllerPath: 'PATH',
+        moduleName: 'clPlatform.TEST'
+      }
+    );
   }
 
 }
+
+export = ComponentGenerator;
