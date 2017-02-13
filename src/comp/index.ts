de2ac96e81db7e9b3086ff3a1f78c97155af7205
Ben Southgate <bsouthga@gmail.com>
2017-02-13 14:49:18 -0500

    rename

---
+++ src/comp/index.ts
@@ -0,0 +1,14 @@
+import Generator = require('yeoman-generator');
+
+
+
+/**
+ * old style commonjs export for yeoman
+ */
+export = class extends Generator {
+
+  method1() {
+    console.log('component');
+  }
+
+}
