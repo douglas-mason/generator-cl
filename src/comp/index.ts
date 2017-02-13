594dc18bb5b22aa6e127a16e4cbb769e84d7ce07
Ben Southgate <bsouthga@gmail.com>
2017-02-13 16:01:05 -0500

    working component generator

--- src/comp/index.ts
+++ src/comp/index.ts
@@ -1,33 +1,70 @@
 import GeneratorClass = require('yeoman-generator');
 import * as _ from 'lodash';
+import * as mkdirp from 'mkdirp';
+import * as path from 'path';
+
 
 
 class ComponentGenerator extends GeneratorClass {
 
   options: {
     name: string;
+    module: string;
   };
 
   constructor(args: any, opts: any) {
     super(args, opts);
     this.argument('name', { type: String, required: true });
+    this.argument('module', { type: String, required: true });
   }
 
-  writing() {
+  async writing() {
     const name = this.options.name;
-    console.log(`In writing method -- ${name}`);
-    this.fs.copyTpl(
-      this.templatePath('template.component.ts.template'),
-      this.destinationPath(`test/${name}.component.ts`),
-      {
-        componentName: name,
-        componentNameCamelCase: _.camelCase(name),
-        controllerName: `${_.capitalize(_.camelCase(name))}Controller`,
-        templateUrl: 'TEST',
-        baseControllerPath: 'PATH',
-        moduleName: 'clPlatform.TEST'
-      }
-    );
+    const cwd = process.cwd();
+    const folderPath = path.join(cwd, name);
+    const appClientIndex = folderPath.indexOf('app/client');
+
+    if (appClientIndex === -1) {
+      throw new Error(`Can only create a component in app/client`);
+    }
+    const appClientBase = folderPath.split('app/client/')[1];
+
+    mkdirp.sync(folderPath);
+
+    const templateUrl = `views/${appClientBase}/${name}.view.html`;
+    const pascalCase = (str: string) => {
+      const camel = _.camelCase(str);
+      return camel.charAt(0).toUpperCase() + camel.slice(1);
+    };
+
+
+    const baseControllerPath = `${'../'.repeat(appClientBase.split('/').length)}_shared/base-controller`;
+
+
+    const data = {
+      componentName: name,
+      componentNameCamelCase: _.camelCase(name),
+      controllerName: `${pascalCase(name)}Controller`,
+      templateUrl,
+      baseControllerPath,
+      moduleName: 'clPlatform.' + this.options.module
+    };
+
+    /**
+     * copy + hydrate templates
+     */
+    ;[
+      'component.ts',
+      'styles.scss',
+      'view.html'
+    ].forEach(filetype => {
+      this.fs.copyTpl(
+        this.templatePath(`template.${filetype}.template`),
+        path.join(folderPath, `${name}.${filetype}`),
+        data
+      );
+    });
+
   }
 
 }
