339ca8fa4acde4579878bbc1587c1f44fba6b4f1
Ben Southgate <bsouthga@gmail.com>
2017-07-25 14:37:10 -0400

    feat: add react/redux generators

---
+++ src/react/index.ts
@@ -0,0 +1,58 @@
+import GeneratorClass = require('yeoman-generator');
+import * as _ from 'lodash';
+import * as mkdirp from 'mkdirp';
+import * as path from 'path';
+import * as fs from 'fs';
+
+
+
+export = class ReactGenerator extends GeneratorClass {
+
+  options: {
+    name: string;
+  };
+
+  constructor(args: any, opts: any) {
+    super(args, opts);
+    this.argument('name', { type: String, required: true });
+  }
+
+  async writing() {
+    const name = this.options.name;
+    const cwd = process.cwd();
+    const folderPath = path.join(cwd, name);
+
+    mkdirp.sync(folderPath);
+
+    const pascalCase = (str: string) => {
+      const camel = _.camelCase(str);
+      return camel.charAt(0).toUpperCase() + camel.slice(1);
+    };
+
+
+    const data = {
+      name,
+      kebabName: _.kebabCase(name),
+      pascalName: pascalCase(name),
+      camelName: _.camelCase(name),
+      allCapsName: _.snakeCase(name).toUpperCase()
+    };
+
+    /**
+     * copy + hydrate templates
+     */
+    fs.readdirSync(path.join(__dirname, 'templates')).forEach(templateFile => {
+      const portions = templateFile.split('.');
+      portions.pop(); // remove .template
+      const suffix = portions.join('.');
+
+      this.fs.copyTpl(
+        this.templatePath(templateFile),
+        path.join(folderPath, `${name}.${suffix}`),
+        data
+      );
+    });
+
+  }
+
+}
